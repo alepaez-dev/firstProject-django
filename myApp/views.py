f31c6fbf9054cdb74ad84545e1f0a67e20d92123
Alejandra Paez <alepaezdev@gmail.com>
2023-08-23 22:56:56 -0600

    feat: clase 2

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Importar HttpResponse
+from django.http import HttpResponse
+
+
+# Las vistas son funciones
+
+
+# Cliente -> Pide -> Request
+# Servidor -> Responde -> Response
+def bienvenida(request):
+    # Responder
+    return HttpResponse("Bienvenido koders!!")
+
+
+def despedida(request):
+    return HttpResponse("Despedida")
+
+
+def saludo(request):
+    return HttpResponse("Saludando a koders")
+
+
+def saludar_con_nombre(request, nombre):
+    return HttpResponse(f"Hola {nombre}")
